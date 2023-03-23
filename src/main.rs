2fe8b0089af7c7f80a72f86d76d0ce1de1ba90e5
yuri-gagarin <ymivanov@icloud.com>
2023-03-23 23:58:18 +0200

    game states

--- src/main.rs
+++ src/main.rs
@@ -46,36 +46,51 @@ async fn main() {
     // draw the ball ///
     balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32)));
     loop {
-        if is_key_pressed(KeyCode::Space) {
-            balls.push(Ball::new(vec2(screen_width() * 0.5f32, screen_height() * 0.5f32)));
-        }
-        player.update(get_frame_time());
-        // ball //
-        for ball in balls.iter_mut() {
-            ball.update(get_frame_time());
-        }
-        for ball in balls.iter_mut() {
-            resolve_collision(&mut ball.rect, &mut ball.vector, &player.rect);
-            for block in blocks.iter_mut() {
-                if resolve_collision(&mut ball.rect, &mut ball.vector, &block.rect) {
-                    block.lives -= 1;
-                    if  block.lives == 0 {
-                        score += 10; 
+        // start of the game state //
+        match game_state {
+            GameState::Menu => {
+                if is_key_pressed(KeyCode::Space) {
+                    game_state = GameState::Game;
+                }
+            }
+            GameState::Game => {
+                player.update(get_frame_time());
+                // update game balls //
+                for ball in balls.iter_mut() {
+                    ball.update(get_frame_time());
+                }
+                for ball in balls.iter_mut() {
+                    resolve_collision(&mut ball.rect, &mut ball.vector, &player.rect);
+                    for block in blocks.iter_mut() {
+                        if resolve_collision(&mut ball.rect, &mut ball.vector, &block.rect) {
+                            block.lives -= 1;
+                            if  block.lives == 0 {
+                                score += 10; 
+                            }
+                            score += 1;
+                        }
                     }
-                    score += 1;
+                }
+                // ball should exist only if in screen //
+                // calculate players lives //
+                let balls_len = balls.len(); 
+                balls.retain(|ball| ball.rect.y < screen_height());
+                let removed_balls = balls_len - balls.len();
+                if removed_balls > 0 {
+                    player.lives -= 1;
+                }
+                // check if block shoud exists //
+                blocks.retain(|block| block.lives > 0);
+
+            }
+            GameState::Completed | GameState::Failed => {
+                if is_key_pressed(KeyCode::Space) {
+                    // need to set a new game
                 }
             }
         }
-        // ball should exist only if in screen //
-        // calculate players lives //
-        let balls_len = balls.len(); 
-        balls.retain(|ball| ball.rect.y < screen_height());
-        let removed_balls = balls_len - balls.len();
-        if removed_balls > 0 {
-            player.lives -= 1;
-        }
-        // check if block shoud exists //
-        blocks.retain(|block| block.lives > 0);
+        
+        
     
         clear_background(macroquad::prelude::WHITE);
         player.draw();
@@ -92,15 +107,15 @@ async fn main() {
         match game_state {
             GameState::Menu => {
                 helpers::draw_main_text("Press SPACE BAR to start", font, 30u16, macroquad::prelude::BLACK);
-            },
+            }
             GameState::Game => {
                 // draw score text and lives text //
                 helpers::draw_score_text(score, font, 30u16, macroquad::prelude::BLACK);
                 helpers::draw_lives_text(player.lives, font, 30u16, macroquad::prelude::BLACK);
-            },
+            }
             GameState::Completed => {
                 helpers::draw_main_text(&format!("You WON! Score: {}", score), font, 30u16, macroquad::prelude::BLACK);
-            },
+            }
             GameState::Failed => {
                 helpers::draw_main_text(&format!("GAME OVER! Score: {}", score), font, 30u16, macroquad::prelude::BLACK);
             }
